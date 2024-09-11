2b9d0de2bf25025e215b8606f3c97517b6d3db55
READYonHub <readyemail3@gmail.com>
2024-09-11 10:14:44 +0200

    5. Blog App Featured Post Design

--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import styles from './featured.module.css';
+import React from "react";
+import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
   return (
     <div className={styles.container}>
-      Featured
+      <h1 className={styles.title}>
+        <b>Hey, OTTO-MOBILE itt van!</b> Fedezd fel a sztoriaimat és a kreatív öteleteimet.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>CimCIMCIMCIMCIMCIMCIM.</h1>
+          <p className={styles.postDesc}>
+           Ez lesz majd a posztnak a leírása.Ez lesz majd a posztnak a leírása. Ez lesz majd a posztnak a leírása.Ez lesz majd a posztnak a leírása.Ez lesz majd a posztnak a leírása.Ez lesz majd a posztnak a leírása.Ez lesz majd a posztnak a leírása.Ez lesz majd a posztnak a leírása.Ez lesz majd a posztnak a leírása.Ez lesz majd a posztnak a leírása.Ez lesz majd a posztnak a leírása.Ez lesz majd a posztnak a leírása.Ez lesz majd a posztnak a leírása.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Featured
+export default Featured;
