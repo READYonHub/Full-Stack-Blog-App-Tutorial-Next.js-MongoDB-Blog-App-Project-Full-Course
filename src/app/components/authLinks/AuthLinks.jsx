d3ee64965f39bf966cb4f705bd1d325fd87b5616
READYonHub <readyemail3@gmail.com>
2024-10-15 22:22:52 +0200

    12. Next.js MongoDB Prisma Tutorial (App Router) és Prisma Blog App Schema Example

--- src/app/components/authLinks/AuthLinks.jsx
+++ src/app/components/authLinks/AuthLinks.jsx
@@ -4,18 +4,18 @@ import { useState } from "react";
 import styles from "./authLinks.module.css";
 
 import Link from "next/link";
-import { signIn, signOut } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const AuthLinks = () => {
 
   const [open, setOpen] = useState(false)
 
   //temporary
-  const status = "notauthenticated";
+  const {status} = useSession();
   return (
     <>
       {
-        status === "nostauthenticated" ? (
+        status === "unauthenticated" ? (
           <Link href="/login" className={styles.link}>Login</Link>
         ) : (
           <>
