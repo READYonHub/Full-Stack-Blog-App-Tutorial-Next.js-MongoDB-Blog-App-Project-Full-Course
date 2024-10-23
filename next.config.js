81467dd268a85206c3894b7cf238c01691b8ea79
READYonHub <104925582+READYonHub@users.noreply.github.com>
2024-10-23 16:42:44 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+// next.config.js
+module.exports = {
+  basePath: '/Full-Stack-Blog-App-Tutorial-Next.js-MongoDB-Blog-App-Project-Full-Course',
+  assetPrefix: '/Full-Stack-Blog-App-Tutorial-Next.js-MongoDB-Blog-App-Project-Full-Course', // Statikus fájlok betöltéséhez
+};
+
 module.exports = nextConfig
